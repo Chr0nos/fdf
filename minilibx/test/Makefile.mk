314206605d00845539eea09e1e49b3a57c0f78fa
Chr0nos <snicolet@student.42.fr>
2016-01-04 11:55:51 +0000

    ajout de ma version de minilibx qui compile

---
+++ minilibx/test/Makefile.mk
@@ -0,0 +1,24 @@
+
+
+INC=%%%%
+
+INCLIB=$(INC)/../lib
+
+CC=gcc
+
+CFLAGS= -I$(INC) -O3 -I.. -lft
+
+NAME= mlx-test
+SRC = main.c
+OBJ = $(SRC:.c=.o)
+
+all	:$(NAME)
+
+$(NAME)	:$(OBJ)
+	$(CC) -o $(NAME) $(OBJ) -L.. -lmlx -L$(INCLIB) -lXext -lX11 -lm -lft -L/home/adamaru/jours/libft
+
+clean	:
+	rm -f $(NAME) $(OBJ) *~ core *.core
+
+
+re	: clean all
