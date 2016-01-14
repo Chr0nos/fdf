4e2438ba882ff19ac0833bd9d81757614d79972f
Chr0nos <snicolet@student.42.fr>
2016-01-14 21:40:56 +0000

    fix de fixed_point, plus qu a trouver comment ne pas utiliser de double/float

--- fixed_point.c
+++ fixed_point.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 #define FIXED_SCALE 20
+#define FIXED_FMASK ((int)2147483647 >> (32 - FIXED_SCALE))
 #define FIXED_REAL(x) ((x) >> FIXED_SCALE)
-#define FIXED_DEC(x) ((x) & ~(1 << FIXED_SCALE) >> 1)
-#define FIXED_DOUBLE(x) ((double)(x)) / ((double)(1 << FIXED_SCALE))
+#define FIXED_DEC(x) (int)((x) & FIXED_FMASK)
+#define FIXED_DOUBLE(x) (((double)(x)) / ((double)(1 << FIXED_SCALE)))
 #define FIXED_FD(x) (int)((x) * (double)(1 << FIXED_SCALE))
 
+//SRRR RRRR RRRR.DDDD DDDD DDDD DDDD DDDD
 //0000 0000 0000.0000 0000 0000 0000 0000
 //0000 0000 0001.0000 0000 0000 0000 0000
 //1111 1111 1111.0000 0000 0000 0000 0000
@@ -17,6 +19,7 @@ int		main(void)
 
 	x = FIXED_FD((double)42.312f);
 	printf("%d -- %d --- %d\n", FIXED_REAL(x), FIXED_DEC(x), x);
-	printf("%lf\n", FIXED_DOUBLE(x));
+	printf("%lf -- %lf\n", FIXED_DOUBLE(x), FIXED_DOUBLE(FIXED_DEC(x)));
+	printf("%d\n", FIXED_FMASK);
 	return (0);
 }
