e975462363beb4850bb1164b24725d26c586c243
Chr0nos <snicolet@student.42.fr>
2016-01-24 18:25:53 +0100

    test useless

--- gameoflife.c
+++ gameoflife.c
@@ -6,15 +6,25 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/24 14:16:24 by snicolet          #+#    #+#             */
-/*   Updated: 2016/01/24 17:03:13 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/01/24 18:20:49 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "draw.h"
 #include "mlx.h"
 #include <stdlib.h>
+#include <stdio.h>
 #define COLUMNS 64
 #define COLOR_BACKGROUND 0x00402050
+
+static t_point	get_steps(t_mlx *x)
+{
+	t_point		steps;
+
+	steps.x = x->width / COLUMNS;
+	steps.y = x->height / COLUMNS;
+	return (steps);
+}
 
 static void		make_grid(t_mlx *x, int columns)
 {
@@ -30,7 +40,7 @@ static void		make_grid(t_mlx *x, int columns)
 		draw_line(x, &line, COLOR_WHITE);
 	}
 	step = x->height / columns;
-	for (p = 0 ; p < x->height ; p += step)
+	for (p = 0 ; p <= x->height ; p += step)
 	{
 		line = draw_make_line(0, p, x->width, p);
 		draw_line(x, &line, COLOR_WHITE);
@@ -42,8 +52,7 @@ static void		color_box(t_mlx *x, int cx, int cy, int color)
 	t_point		steps;
 	t_rect		r;
 
-	steps.x = x->width / COLUMNS;
-	steps.y = x->height / COLUMNS;
+	steps = get_steps(x);
 	r.start.x = cx * steps.x + 1;
 	r.start.y = cy * steps.y + 1;
 	r.end.x = (cx + 1) * steps.x;
@@ -64,19 +73,22 @@ static t_point	getcase(t_mlx *x, int px, int py)
 	t_point		steps;
 	t_point		p;
 
-	steps.x = x->width / COLUMNS;
-	steps.y = x->height / COLUMNS;
+	steps = get_steps(x);
 	p.x = px / steps.x;
 	p.y = py / steps.y;
 	return (p);
 }
+/*
+static int		getcase_color(t_mlx *x, int cx, int cy)
+{
 
+}
+*/
 static int		mouse(int button, int mx, int my, void *userdata)
 {
 	t_mlx	*x;
 	t_point	c;
 
-	//printf("%d %d %d\n", button, mx, my);
 	x = (t_mlx*)userdata;
 	c = getcase(x, mx, my);
 	if (button == 1)
@@ -89,8 +101,21 @@ static int		mouse(int button, int mx, int my, void *userdata)
 		color_box(x, c.x, c.y, COLOR_BACKGROUND);
 		draw_flush_image(x, x->img);
 	}
-	//display_gol(x);
 	return (0);
+}
+
+static void next_turn(t_mlx *x)
+{
+	t_point			p;
+
+	for (p.y = 0 ; p.y <= COLUMNS ; p.y++)
+	{
+		for (p.x = 0 ; p.x < COLUMNS ; p.x++)
+		{
+			color_box(x, p.x, p.y, COLOR_CYAN);
+		}
+	}
+	draw_flush_image(x, x->img);
 }
 
 static int	keys(int keycode, void *userdata)
@@ -100,8 +125,12 @@ static int	keys(int keycode, void *userdata)
 	x = userdata;
 	if (keycode == 53)
 		display_gol(x);
-	else if (keycode == 'q')
+	else if ((keycode == 'q') || (keycode == 12))
 		exit(0);
+	else if ((keycode == 49) || (keycode == ' '))
+		next_turn(x);
+	else
+		printf("key: %d\n", keycode);
 	return (0);
 }
 
