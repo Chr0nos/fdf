1d9a4fc082fd3ea8a143f9e0bbc1c17b12c4553a
Chr0nos <snicolet@student.42.fr>
2016-02-04 14:01:43 +0100

    sauvegarde

--- grid.c
+++ grid.c
@@ -5,17 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/02/02 12:14:41 by snicolet          #+#    #+#             */
-/*   Updated: 2016/02/03 12:17:11 by snicolet         ###   ########.fr       */
+/*   Created: 2016/02/04 13:57:45 by snicolet          #+#    #+#             */
+/*   Updated: 2016/02/04 14:01:15 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-#include "draw.h"
 
-void	grid(t_mlx *x, t_plist **plist, int color)
+void	grid_init(t_mlx *x, t_plist **plist)
 {
-	(void)x;
-	(void)plist;
-	(void)color;	
+	size_t		line;
+	size_t		col;
+	t_line		l;
+	t_vector	*p1;
+	t_vector	*p2;
+
+	line = 0;
+	while ((plist[line]) && (!(col = 0)))
+	{
+		while (col < plist[line]->size)
+		{
+			p1 = plist[line]->points[col];
+			if (plist[line + 1])
+			{
+				p2 = plist[line + 1]->points[col];
+			}
+			col++;
+		}
+	}
 }
