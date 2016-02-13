6c5969871a2ab09c79ef0dcfae6830a51653f71c
Chr0nos <snicolet@student.42.fr>
2016-02-13 14:22:46 +0100

    opti de draw_px

--- libs/draw/draw_px.c
+++ libs/draw/draw_px.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/29 14:13:28 by snicolet          #+#    #+#             */
-/*   Updated: 2016/02/12 12:57:27 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/02/13 14:22:19 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,8 @@
 void	draw_px(t_mlx *x, const t_point *point, int color)
 {
 	const int	octet = x->img->bpp / 8;
-	int			position;
+	const int	position = (x->img->width * point->y) + (point->x * octet);
 
-	position = (x->img->width * point->y) + (point->x * octet);
 	if (ft_memcmp(x->img->data + position, &color, (unsigned)octet) != 0)
 		ft_memcpy(x->img->data + position, &color, (unsigned)octet);
 }
